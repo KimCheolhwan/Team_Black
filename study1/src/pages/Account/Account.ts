f17a0694b6abf2ca1b2f7684833e87e0d098f390
hyunjoon <hyunjoon0312@gmail.com>
2017-07-16 00:44:27 +0900

    17.07.15 ionic_study

---
+++ study1/src/pages/Account/Account.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-Account',
+  templateUrl: 'Account.html'
+})
+export class AccountPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
